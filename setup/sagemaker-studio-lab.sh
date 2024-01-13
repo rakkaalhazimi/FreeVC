1b33b139791a88fd69f6ff1b56e96ec775416a16
Rakka Alhazimi <rakkakeren@gmail.com>
2024-01-13 18:11:55 +0700

    fix: missing url and torch install index

--- setup/sagemaker-studio-lab.sh
+++ setup/sagemaker-studio-lab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Setup FREEVC for training in Sagemaker Studio Lab Environment
 FREEVC_ENV=free-vc
-DATASET_URL=https://drive.google.com/file/d/1bfByWAngeFzGBFhuqwzW0p80p0DTxK6Z&export=download
-MODEL_URL=https://drive.google.com/file/d/1ci-qtZYSsJYPAMufH-NIsYUWKdSzHhLe&export=download
+DATASET_URL="https://drive.google.com/file/d/1bfByWAngeFzGBFhuqwzW0p80p0DTxK6Z&export=download"
+MODEL_URL="https://drive.google.com/file/d/1ci-qtZYSsJYPAMufH-NIsYUWKdSzHhLe&export=download"
 DATASET_ZIP_FILENAME=dataset.zip
 MODEL_ZIP_FILENAME=model.zip
 MODEL_PATH=logs/freevc
@@ -19,7 +19,7 @@ unzip $MODEL_ZIP_FILENAME -d $MODEL_PATH
 # Setup Environment and Install Dependencies
 conda create --name $FREEVC_ENV python=3.9
 conda activate $FREEVC_ENV
-pip install -r requirements.txt
+pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
 
 
 # Train model
